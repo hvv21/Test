d7b759aa0ab406df99b15ee56c691f67b8544dd8
hvv21 <102385245+hvv21@users.noreply.github.com>
2022-05-12 22:20:34 +0300

    Delete date.js

--- date.js
+++
@@ -1,6 +0,0 @@
-let todayDate = new Date();
-let currYear = todayDate.getFullYear();
-let currMonth = todayDate.getMonth()+1;
-let currDay = todayDate.getDate();
-document.write("Сьогодні: ");
-document.write(currDay + "/" + currMonth + "/" + currYear);
